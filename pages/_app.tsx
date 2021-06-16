b08b21a83c84e112e5e8f15c8352fe32f49b8a3e
Krešimir <kresimir.coko@liferay.com>
2021-06-16 15:38:58 +0200

    chore: add a dynamic title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,35 @@
 import '../styles/main.scss';
 import ClayNav from '@clayui/nav';
 import ClayIcon, {ClayIconSpriteContext} from '@clayui/icon';
+import Head from 'next/head';
 import {useRouter} from 'next/router';
 
 const spritemap = '../images/icons.svg';
 
 function MyApp({Component, pageProps}) {
 	const router = useRouter();
+
 	const currentRoute = router.pathname;
+
+	const title = currentRoute.substr(1);
+
+	const capitalizedTitle = title
+		? title.charAt(0).toUpperCase() + title.slice(1)
+		: 'Dashboard';
 
 	return (
 		<>
+			<Head>
+				<title>{capitalizedTitle}</title>
+
+				<meta
+					name="description"
+					content="Liferay automatic code improvement tool"
+				/>
+
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
 			<ClayIconSpriteContext.Provider value={spritemap}>
 				<header>
 					<ClayNav>
