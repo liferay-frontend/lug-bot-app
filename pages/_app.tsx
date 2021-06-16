b365bfb6c1df7059cd63d1f67d01bfc7390aa045
Krešimir <kresimir.coko@liferay.com>
2021-06-16 09:38:04 +0200

    chore: move layout to a centralized file and use context for icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,64 @@
-import '../styles/main.scss'
+import '../styles/main.scss';
+import ClayNav from '@clayui/nav';
+import ClayIcon, {ClayIconSpriteContext} from '@clayui/icon';
+import {useRouter} from 'next/router';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const spritemap = '../images/icons.svg';
+
+function MyApp({Component, pageProps}) {
+	const router = useRouter();
+	const currentRoute = router.pathname;
+
+	return (
+		<>
+			<ClayIconSpriteContext.Provider value={spritemap}>
+				<header>
+					<ClayNav>
+						<ClayNav.Item>
+							<ClayNav.Link
+								active={currentRoute === '/'}
+								href="/"
+							>
+								<ClayIcon symbol="sheets" />
+
+								{'Dashboard'}
+							</ClayNav.Link>
+						</ClayNav.Item>
+
+						<ClayNav.Item>
+							<ClayNav.Link
+								active={currentRoute === '/jobs'}
+								href="/jobs"
+							>
+								<ClayIcon symbol="code" />
+
+								{'Jobs'}
+							</ClayNav.Link>
+						</ClayNav.Item>
+
+						<ClayNav.Item>
+							<ClayNav.Link
+								active={currentRoute === '/report'}
+								href="/report"
+							>
+								<ClayIcon symbol="analytics" />
+
+								{'Report'}
+							</ClayNav.Link>
+						</ClayNav.Item>
+					</ClayNav>
+				</header>
+
+				<main>
+					<Component {...pageProps} />
+				</main>
+
+				<footer>
+					<span>Footer</span>
+				</footer>
+			</ClayIconSpriteContext.Provider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
