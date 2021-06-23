1103926c778487ebd6554fd93eb4258f19d5232b
Krešimir <kresimir.coko@liferay.com>
2021-06-23 14:38:47 +0200

    chore: Add url to job title

--- pages/api/project.ts
+++ pages/api/project.ts
@@ -1,6 +1,7 @@
-export default (req, res) => {
+export default (_req, res) => {
 	res.status(200).json({
 		location: 'git@github.com:gamerson/evp-portlet-upgrade-webhook.git',
 		name: 'EVP Portlet',
+		url: 'https://github.com/gamerson/evp-portlet-upgrade-webhook',
 	});
 };
