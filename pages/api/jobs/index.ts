fbec32c6824fe96d90a5af841aaad3f101a23976
Krešimir <kresimir.coko@liferay.com>
2021-06-29 15:01:49 +0200

    chore: rename dummy date import

--- pages/api/jobs/index.ts
+++ pages/api/jobs/index.ts
@@ -1,9 +1,9 @@
-import ALL_JOBS from '../../../dummy-data';
+import JOBS_AND_PROJECTS from '../../../dummy-data';
 
 export default async function (_req, res) {
 	// REPLACE WITH LUGBOT API
 	const jobs = await new Promise((res) => {
-		res(ALL_JOBS);
+		res(JOBS_AND_PROJECTS);
 	});
 
 	res.status(200).json(jobs);
