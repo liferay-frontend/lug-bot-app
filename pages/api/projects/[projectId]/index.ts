5562b4257aab1b2efe01ba24c0da597613038298
Krešimir <kresimir.coko@liferay.com>
2021-07-05 13:27:57 +0200

    chore: remove files related to projects

--- pages/api/projects/[projectId]/index.ts
+++
@@ -1,9 +0,0 @@
-import PROJECTS from '../../../../dummy-data';
-
-export default (req, res) => {
-	const {projectId} = req.query;
-
-	const project = PROJECTS.find((project) => project.id === projectId);
-
-	res.status(200).json(project);
-};
