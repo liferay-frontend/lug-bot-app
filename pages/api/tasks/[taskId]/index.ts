c96851a1f7c95ea2ed93d356c8e8b73aeee5aa12
Krešimir <kresimir.coko@liferay.com>
2021-07-05 13:28:44 +0200

    chore: adapt and move files around to support only single project not multiple

--- pages/api/tasks/[taskId]/index.ts
+++ pages/api/tasks/[taskId]/index.ts
@@ -1,15 +1,22 @@
-import PROJECTS from '../../../../dummy-data';
+import PROJECT from '../../../../dummy-data';
 
 export default (req, res) => {
 	const {taskId} = req.query;
 
-	const project = PROJECTS[0];
+	if (req.method === 'POST') {
+		const task = PROJECT.runningTasks.find((task) => task.id === taskId);
 
-	const task = [
-		...project.completedTasks,
-		...project.pendingTasks,
-		...project.runningTasks,
-	].find((task) => task.id === taskId);
+		PROJECT.runningTasks.splice(PROJECT.runningTasks.indexOf(task), 1);
+		PROJECT.pendingTasks.push(req.body);
 
-	res.status(200).json(task);
+		res.status(200).json(req.body);
+	} else {
+		const task = [
+			...PROJECT.completedTasks,
+			...PROJECT.pendingTasks,
+			...PROJECT.runningTasks,
+		].find((task) => task.id === taskId);
+
+		res.status(200).json(task);
+	}
 };
