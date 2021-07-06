3da09de9d2c90201bcab6c8a4a46a8f017b215c4
Krešimir <kresimir.coko@liferay.com>
2021-07-06 15:24:19 +0200

    chore: move formatDuration util outside of the component

---
+++ utils/formatDuration.ts
@@ -0,0 +1,20 @@
+export default function formatDuration(duration: number) {
+	let seconds: string | number = Math.floor((duration / 1000) % 60);
+	let minutes: string | number = Math.floor((duration / (1000 * 60)) % 60);
+	const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+	minutes = minutes < 10 ? `${hours > 0 ? '0' : ''}${minutes}` : minutes;
+	seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+	if (hours > 0) {
+		return `${hours}h ${minutes}m`;
+	}
+
+	if (minutes !== '00') {
+		return `${minutes}m`;
+	}
+
+	if (seconds) {
+		return `${seconds}s`;
+	}
+}
