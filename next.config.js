57d45ab96674e8fd549026e69a103aec557aac58
Krešimir <kresimir.coko@liferay.com>
2021-07-07 13:07:45 +0200

    fix: make baseline redirect more stable

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+	async redirects() {
+		return [
+			{
+				destination: '/tasks',
+				permanent: true,
+				source: '/',
+			},
+		];
+	},
 	sassOptions: {
 		includePaths: require('@clayui/css').includePaths,
 	},
