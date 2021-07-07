cd65a750a99c3d6f8f11c1faa4fcf964e5e309fa
Krešimir <kresimir.coko@liferay.com>
2021-07-07 10:40:47 +0200

    chore: clear interval

--- components/CountUp.tsx
+++ components/CountUp.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const CountUp = ({startTime}) => {
-	const [time, setTime] = React.useState(new Date(startTime).getTime());
+	const [time, setTime] = useState(new Date(startTime).getTime());
 
-	React.useEffect(() => {
-		setInterval(() => {
+	useEffect(() => {
+		const interval = setInterval(() => {
 			setTime((time) => time + 1000);
 		}, 1000);
+
+		return () => clearInterval(interval);
 	}, []);
 
 	return <span>{new Date(time).toISOString().substr(11, 8)}</span>;
