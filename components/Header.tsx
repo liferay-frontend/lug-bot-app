fbe9f673ea982b8c80505a2fad7e7df7e3119be6
Krešimir <kresimir.coko@liferay.com>
2021-07-13 14:30:39 +0200

    feat: add proper shutdown fetch

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,34 +2,37 @@ import ClayButton from '@clayui/button';
 import ClayLink from '@clayui/link';
 import ClayNavigationBar from '@clayui/navigation-bar';
 
-const Header = ({currentRoute}) => (
-	<header>
-		<ClayNavigationBar inverted triggerLabel="Tasks">
-			<ClayLink className="navbar-brand" href="/">
-				{'Lugbot'}
-			</ClayLink>
+import API_ENDPOINT from '../constants/apiEndpoint';
 
-			<ClayNavigationBar.Item active={currentRoute === '/tasks'}>
-				<ClayLink
-					className="nav-link"
-					displayType="unstyled"
-					href="/tasks"
-				>
-					{'Dashboard'}
+export default function Header({currentRoute}) {
+	return (
+		<header>
+			<ClayNavigationBar inverted triggerLabel="Tasks">
+				<ClayLink className="navbar-brand" href="/">
+					{'Lugbot'}
 				</ClayLink>
-			</ClayNavigationBar.Item>
 
-			<ClayButton
-				className="align-self-center btn-danger ml-auto"
-				onClick={() =>
-					confirm('You are about to stop Lugbot. Are you sure?')
-				}
-				small
-			>
-				{'Stop Lugbot'}
-			</ClayButton>
-		</ClayNavigationBar>
-	</header>
-);
+				<ClayNavigationBar.Item active={currentRoute === '/tasks'}>
+					<ClayLink
+						className="nav-link"
+						displayType="unstyled"
+						href="/tasks"
+					>
+						{'Dashboard'}
+					</ClayLink>
+				</ClayNavigationBar.Item>
 
-export default Header;
+				<ClayButton
+					className="align-self-center btn-danger ml-auto"
+					onClick={() =>
+						fetch(`${API_ENDPOINT}/shutdown`, {method: 'POST'})
+					}
+					small
+				>
+					{'Stop Lugbot'}
+				</ClayButton>
+			</ClayNavigationBar>
+			);
+		</header>
+	);
+}
