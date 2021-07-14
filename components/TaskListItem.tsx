4e8d69f9b03d8325d1e46de9c6dd444627f3f4b6
Krešimir <kresimir.coko@liferay.com>
2021-07-14 15:15:05 +0200

    sf: remove console.log and remove curly braces

--- components/TaskListItem.tsx
+++ components/TaskListItem.tsx
@@ -7,48 +7,41 @@ import STATES from '../constants/taskStates';
 import formatDuration from '../utils/formatDuration';
 import CountUp from './CountUp';
 
-const TaskListItem = ({task, taskState}) => {
-	console.log(
-		'date: ',
-		new Date(task.finishTime).getTime() - new Date(task.startTime).getTime()
-	);
-	
-	return (
-		<ClayList.Item flex className="align-items-center" key={task.id}>
-			<ClayList.ItemField expand>
-				<Link href={`/tasks/${task.id}`} passHref>
-					<ClayList.ItemTitle>{task.name}</ClayList.ItemTitle>
-				</Link>
+const TaskListItem = ({task, taskState}) => (
+	<ClayList.Item flex className="align-items-center" key={task.id}>
+		<ClayList.ItemField expand>
+			<Link href={`/tasks/${task.id}`} passHref>
+				<ClayList.ItemTitle>{task.name}</ClayList.ItemTitle>
+			</Link>
 
-				{task.totalRecommendations && (
-					<ClayList.ItemText>
-						{`${task.totalRecommendations} Recommendations`}
-					</ClayList.ItemText>
+			{task.totalRecommendations && (
+				<ClayList.ItemText>
+					{`${task.totalRecommendations} Recommendations`}
+				</ClayList.ItemText>
+			)}
+		</ClayList.ItemField>
+
+		{taskState === STATES.runningState && (
+			<ClayList.ItemField>
+				<CountUp startTime={task.startTime} />
+			</ClayList.ItemField>
+		)}
+
+		{taskState === STATES.completedState && (
+			<ClayList.ItemField>
+				{formatDuration(
+					new Date(task.finishTime).getTime() -
+						new Date(task.startTime).getTime()
 				)}
 			</ClayList.ItemField>
+		)}
 
-			{taskState === STATES.runningState && (
-				<ClayList.ItemField>
-					<CountUp startTime={task.startTime} />
-				</ClayList.ItemField>
-			)}
-
-			{taskState === STATES.completedState && (
-				<ClayList.ItemField>
-					{formatDuration(
-						new Date(task.finishTime).getTime() -
-							new Date(task.startTime).getTime()
-					)}
-				</ClayList.ItemField>
-			)}
-
-			<ClayList.ItemField>
-				<ClayLabel displayType={STATES.byId[task.state].displayType}>
-					{STATES.byId[task.state].label}
-				</ClayLabel>
-			</ClayList.ItemField>
-		</ClayList.Item>
-	);
-};
+		<ClayList.ItemField>
+			<ClayLabel displayType={STATES.byId[task.state].displayType}>
+				{STATES.byId[task.state].label}
+			</ClayLabel>
+		</ClayList.ItemField>
+	</ClayList.Item>
+);
 
 export default TaskListItem;
