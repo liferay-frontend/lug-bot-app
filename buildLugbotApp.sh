08d8b20d5dda2c6ad8b323436e725be297d4d0b9
Gregory Amerson <gregory.amerson@liferay.com>
2021-07-14 23:41:54 -0500

    scripts for building lugbot/lugbot-app to better integrate it with lugbot backend

---
+++ buildLugbotApp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+trap "exit" INT
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+GIT_SHA=$(git rev-parse --short HEAD)
+
+SHORT_VERSION=$(cat "$DIR/version.txt")
+
+VERSION=${SHORT_VERSION}-$GIT_SHA
+
+export DOCKER_BUILDKIT=1
+
+function assert_success () {
+	if [ $? -ne 0 ]; then
+		echo "$1 failed"
+		exit 1
+	else
+		echo "$1 succeeded"
+	fi
+}
+
+function build_app () {
+	docker build \
+		-f Dockerfile \
+		-t lugbot/lugbot-app:$VERSION \
+		-t lugbot/lugbot-app:$SHORT_VERSION \
+		-t lugbot/lugbot-app:latest \
+		--build-arg VERSION=${VERSION} \
+		--progress plain \
+		.
+
+	assert_success "lugbot/lugbot-app:$VERSION build"
+}
+
+function push_images {
+	docker push lugbot/lugbot-app:$VERSION
+
+	assert_success "lugbot/lugbot-app:$VERSION push"
+
+	docker push lugbot/lugbot-app:$SHORT_VERSION
+
+	assert_success "lugbot/lugbot-app:$SHORT_VERSION push"
+
+	docker push lugbot/lugbot-app:latest
+
+	assert_success "lugbot/lugbot-app:latest push"
+}
+
+build_app
+
+#push_images
